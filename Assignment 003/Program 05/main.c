be50fceb72884cbe19739733d4f060ab64f0faef
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-08-07 10:48:14 +0000

    Add Program 05

---
+++ Assignment 003/Program 05/main.c
@@ -0,0 +1,22 @@
+//	e)	Write a program to swap two numbers without using 3rd variable.
+
+#include <stdio.h>
+int main()
+{
+	int x, y;
+
+	printf("Enter x : ");
+	scanf("%d", &x);
+	printf("Enter y : ");
+	scanf("%d", &y);
+
+	x = x + y;
+	y = x - y;
+	x = x - y;
+
+	printf("\nAfter swapping...\n");
+	printf("Value of x = %d\n", x);
+	printf("Value of y = %d\n", y);
+
+	return 0;
+}
