4e06a506bb9e9501e1f13c8ad73e66b5abb218ff
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-08-14 10:49:33 +0000

    Add P7

---
+++ Assignment 005/Program 07/main.c
@@ -0,0 +1,25 @@
+//  g)  Write a program to find the absolute value
+//      of a number entered through the keyboard.
+
+#include <stdio.h>
+int main() {
+    int n, abs;
+
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+    if (n >= 0)
+        abs = n;
+    else
+        abs = -n;
+
+    printf("Absolute value = %d\n", abs);
+
+    return 0;
+}
+
+/*
+Output :
+Enter a number : -87
+Absolute value = 87
+*/
