f1833dda87dc324b2eafd9accfb9de1deaa2dc22
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-08-27 05:34:27 +0000

    nn

--- Assignment 008/Program 05/main.c
+++ Assignment 008/Program 05/main.c
@@ -1,8 +1,19 @@
-//  e)  Question
+//  e)  Fibonacci Number
 
-#include <math.h>
 #include <stdio.h>
 int main() {
+    int a = 0, b = 1, c, n;
+
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+
     return 0;
 }
 
