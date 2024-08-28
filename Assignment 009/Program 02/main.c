0da13cc7ec39e9f0c6ac7897ac5c8f7240240f82
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-08-28 09:21:58 +0000

    P2

---
+++ Assignment 009/Program 02/main.c
@@ -0,0 +1,16 @@
+//  a)  Print numbers from 10 to 1 in descending order.
+
+#include <stdio.h>
+int main() {
+    for (int i = 10; i >= 1; i--) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+/*
+-----------Output------------
+10 9 8 7 6 5 4 3 2 1
+*/
