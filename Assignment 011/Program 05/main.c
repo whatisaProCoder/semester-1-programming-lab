a317a28a667dc44314470ab464623a0ad2c9cb33
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-09-04 10:11:25 +0000

    P5

---
+++ Assignment 011/Program 05/main.c
@@ -0,0 +1,38 @@
+/*
+e)  Print the following pattern with the
+    number of rows taken from the user.
+        *
+       ***
+      *****
+     *******
+    *********
+*/
+
+#include <stdio.h>
+int main() {
+    int n;
+
+    printf("Enter number of rows : ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+/*
+-----------Output------------
+Enter number of rows : 4
+   *
+  ***
+ *****
+*******
+*/
