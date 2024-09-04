205e0156eb7e9b6dd968a3c02fbbb2b264b51c60
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-09-04 09:31:32 +0000

    P3

---
+++ Assignment 011/Program 03/main.c
@@ -0,0 +1,32 @@
+/*
+c)  Take any positive number from the user and print
+    -> the sum of its digits
+    -> the number of its digits;
+*/
+
+#include <stdio.h>
+int main() {
+    int n, sum = 0, num = 0;
+
+    printf("Enter +ve integer : ");
+    scanf("%d", &n);
+
+    while (n > 0) {
+        int d = n % 10;
+        sum += d;
+        num++;
+        n = n / 10;
+    }
+
+    printf("Sum of digits = %d\n", sum);
+    printf("Number of digits = %d\n", num);
+
+    return 0;
+}
+
+/*
+-----------Output------------
+Enter +ve integer : 12345
+Sum of digits = 15
+Number of digits = 5
+*/
