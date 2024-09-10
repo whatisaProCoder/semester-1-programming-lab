790d89ca87446bd5becb7e8522e04d18bb9dda98
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-09-10 05:32:49 +0000

    P3

---
+++ Assignment 012/Program 03/main.c
@@ -0,0 +1,34 @@
+/*
+c)  WAP in C to print the first n numbers of the Fibonacci sequence.
+    Define the Fibonacci function and call it to print the numbers.
+*/
+
+#include <stdio.h>
+int fibonacci(int n) {
+    int a = 0, b = 1, c;
+    printf("%d terms of Fibonacci Sequence..\n", n);
+    for (int i = 1; i <= n; i++) {
+        c = a + b;
+        printf("%d ", a);
+        a = b;
+        b = c;
+    }
+    printf("\n");
+}
+int main() {
+    int n;
+
+    printf("Enter n : ");
+    scanf("%d", &n);
+
+    fibonacci(n);
+
+    return 0;
+}
+
+/*
+-----------Output------------
+Enter n : 5
+5 terms of Fibonacci Sequence..
+0 1 1 2 3
+*/
