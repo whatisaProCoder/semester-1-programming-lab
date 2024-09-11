1c74f85ae63c51ea6b00615a84105286d73cd7bf
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-09-11 09:10:50 +0000

    P2

---
+++ Assignment 013/Program 02/main.c
@@ -0,0 +1,30 @@
+/*
+a)  Print the given pattern.
+*/
+
+#include <stdio.h>
+int main() {
+    int n, count = 1;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = n; i >= 0; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+/*
+-----------Output------------
+Enter number of rows: 5
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+*/
