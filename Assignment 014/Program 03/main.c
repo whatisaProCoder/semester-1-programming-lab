3bd3ee0ce9d04dedb728a39b5c89de9b5843a31a
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-09-17 05:30:20 +0000

    P3

---
+++ Assignment 014/Program 03/main.c
@@ -0,0 +1,29 @@
+/*
+c)  WAP in C program to define a function
+    that returns the n-th Fibonacci number.
+    Define the function recursively.
+*/
+
+#include <stdio.h>
+int fib(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+int main() {
+    int n;
+
+    printf("Enter n : ");
+    scanf("%d", &n);
+
+    printf("%d-th Fibonacci Number = %d\n", n, fib(n));
+
+    return 0;
+}
+
+/*
+-----------Output------------
+Enter n : 9
+9-th Fibonacci Number = 34
+*/
