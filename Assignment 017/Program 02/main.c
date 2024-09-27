93d40ac9361992783570d53f1f3cc78302705440
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-09-27 15:39:08 +0000

    P2

--- Assignment 017/Program 02/main.c
+++ Assignment 017/Program 02/main.c
@@ -1,13 +1,29 @@
 /*
-b)  jaa nije kor
+b)  Find the square and cube of any number. Use two different functions for square and cube.
 */
 
 #include <stdio.h>
+int square(int n) {
+    return n * n;
+}
+int cube(int n) {
+    return n * n * n;
+}
 int main() {
+    int n;
+
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+    printf("Square of %d = %d\n", n, square(n));
+    printf("Cube of %d = %d\n", n, cube(n));
+
     return 0;
 }
 
 /*
 -----------Output------------
-
+Enter a number : 3
+Square of 3 = 9
+Cube of 3 = 27
 */
