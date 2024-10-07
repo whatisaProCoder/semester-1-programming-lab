58dd7f71ecce7798655bdf40352aafae38d45b50
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-10-07 12:44:06 +0000

    P2

---
+++ Assignment 018/Program 01/main.c
@@ -0,0 +1,24 @@
+/*
+a)  Write a C program to define a function that
+    converts the given temperature in Celcius to
+    Fahrenheit. Call the function and convert the
+    temperature 100 degree Celcius to Fahrenheit.
+*/
+
+#include <stdio.h>
+float getF(float C) { return (C * 9.0 / 5.0) + 32; }
+int main() {
+  float C = 100;
+  float F = getF(C);
+
+  printf("Temperature in Celcius: %.2f\n", C);
+  printf("Temperature in Fahrenheit: %.2f\n", F);
+
+  return 0;
+}
+
+/*
+-----------Output------------
+Temperature in Celcius: 100.00
+Temperature in Fahrenheit: 212.00
+*/
---
+++ Assignment 018/Program 02/main.c
@@ -0,0 +1,25 @@
+/*
+b)  Write a program in C where a function named
+    circlearea is defined which takes the radius of
+    a circle as input and returns the area of the
+    circle. Call the function to compute and print
+    the area of a circle with radius 7.33
+*/
+
+#include <stdio.h>
+float circlearea(float r) { return 3.14 * r * r; }
+int main() {
+  float r = 7.33;
+  float area = circlearea(r);
+
+  printf("Radius of circle: %.2f\n", r);
+  printf("Area of circle: %.2f\n", area);
+
+  return 0;
+}
+
+/*
+-----------Output------------
+Radius of circle: 7.33
+Area of circle: 168.71
+*/
