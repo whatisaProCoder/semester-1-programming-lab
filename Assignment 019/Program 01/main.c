6104bffccc8e2a4897ec833db2d3860c8daa568d
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-10-29 04:52:58 +0000

    P1

--- Assignment 019/Program 01/main.c
+++ Assignment 019/Program 01/main.c
@@ -6,12 +6,58 @@ a)  Create a structure named Book to store book details
 */
 
 #include <stdio.h>
+#include <string.h>
 struct Book {
-
+    char author[30];
+    char title[30];
+    float price;
 };
-int main() {}
+int main() {
+    struct Book books[3];
+    printf("Enter details about 3 books...\n");
+    for (int i = 0; i < 3; i++) {
+        printf("For book %d :\n", i + 1);
+        printf("Author? ");
+        scanf("%s", books[i].author);
+        printf("Title? ");
+        scanf("%s", books[i].title);
+        printf("Price? ");
+        scanf("%f", &books[i].price);
+    }
+
+    int max_price = 0;
+    int req_index = -1;
+    for (int i = 0; i < 3; i++) {
+        if (books[i].price > max_price) {
+            max_price = books[i].price;
+            req_index = i;
+        }
+    }
+
+    printf("The most expensive book in the collection is...\n");
+    struct Book exp_book = books[req_index];
+    printf("Author : %s\n", exp_book.author);
+    printf("Title  : %s\n", exp_book.title);
+    printf("Price : %.2f\n", exp_book.price);
+}
 
 /*
 -----------Output------------
-
+Enter details about 3 books...
+For book 1 :
+Author? Author1
+Title? Title1
+Price? 800
+For book 2 :
+Author? Author2
+Title? Title2
+Price? 900
+For book 3 :
+Author? Author3
+Title? Title3
+Price? 600
+The most expensive book in the collection is...
+Author : Author2
+Title  : Title2
+Price : 900.00
 */
