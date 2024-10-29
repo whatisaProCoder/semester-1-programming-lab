33c1c38d270edb763baab9bf6b13ce7262b1751f
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-10-29 04:24:40 +0000

    sagar

---
+++ Assignment 019/Program 01/main.c
@@ -0,0 +1,17 @@
+/*
+a)  Create a structure named Book to store book details
+    like author, title, price. WAP to input details for
+    three books. Find the most expensive book and display
+    its info.
+*/
+
+#include <stdio.h>
+struct Book {
+
+};
+int main() {}
+
+/*
+-----------Output------------
+
+*/
