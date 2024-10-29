5fbc7dcb9a2ad61bc65172aae7bff624ed4829f1
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-10-29 04:55:49 +0000

    sagar

--- Assignment 019/Program 02/main.c
+++ Assignment 019/Program 02/main.c
@@ -1,8 +1,13 @@
 /*
-a)
+b)  Design a structure named Car to store details like
+    company name, model, price. WAP to input data for
+    four cars. Find the lowest price car details.
 */
 
 #include <stdio.h>
+struct Car {
+    
+}
 int main() {}
 
 /*
