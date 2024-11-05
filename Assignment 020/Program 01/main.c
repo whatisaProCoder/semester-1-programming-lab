8f3b60b3fa171abefbf37d8bb380e0aaef41a65e
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-05 05:00:30 +0000

    P2

--- Assignment 020/Program 01/main.c
+++ Assignment 020/Program 01/main.c
@@ -5,18 +5,14 @@ a)  WAP in C to display all the values in an array in reversed order.
 #include <stdio.h>
 int main() {
     int a[] = {1, 2, 3, 4, 5};
-    int sz = 5;
+    int sz = sizeof(a) / sizeof(a[0]);
     printf("In reversed order...\n");
-    for (int i = sz - 1; i >= 0; i--) printf("%d\n", a[i]);
+    for (int i = sz - 1; i >= 0; i--) printf("%d ", a[i]);
     return 0;
 }
 
 /*
 -----------Output------------
 In reversed order...
-5
-4
-3
-2
-1
+5 4 3 2 1
 */
--- Assignment 020/Program 02/main.c
+++ Assignment 020/Program 02/main.c
@@ -1,14 +1,22 @@
 /*
-b)  
+b)  WAP in C to count the total number of duplicate elements in an array.
 */
 
 #include <stdio.h>
 int main() {
-    
+    int a[] = {1, 2, 3, 4, 5, 6, 5, 4, 2};
+    int sz = sizeof(a) / sizeof(a[0]);
+    int num = 0;
+    for (int i = 0; i < sz - 1; i++) {
+        for (int j = i + 1; j < sz; j++) {
+            if (a[i] == a[j]) num++;
+        }
+    }
+    printf("Number of duplicates : %d\n", num);
     return 0;
 }
 
 /*
 -----------Output------------
-
+Number of duplicates : 3
 */
