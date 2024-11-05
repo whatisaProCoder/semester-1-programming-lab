3964f9ead5e2e1b58a3d475746c16b9222560a2c
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-05 04:47:08 +0000

    P1

--- Assignment 020/Program 01/main.c
+++ Assignment 020/Program 01/main.c
@@ -1,14 +1,22 @@
 /*
-a)  
+a)  WAP in C to display all the values in an array in reversed order.
 */
 
 #include <stdio.h>
 int main() {
-    
+    int a[] = {1, 2, 3, 4, 5};
+    int sz = 5;
+    printf("In reversed order...\n");
+    for (int i = sz - 1; i >= 0; i--) printf("%d\n", a[i]);
     return 0;
 }
 
 /*
 -----------Output------------
-
+In reversed order...
+5
+4
+3
+2
+1
 */
