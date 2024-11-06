f6ac54acce48d50c2787eb778d399c2f8f717d6b
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-06 09:25:30 +0000

    P1

---
+++ Assignment 021/Program 01/main.c
@@ -0,0 +1,16 @@
+/*
+a)  Take n (number of elements in the array) from user and 
+    then take n elements (int or float) from user and store 
+    it in the array.
+*/
+
+#include <stdio.h>
+int main() {
+    int n
+}
+
+/*
+-----------Output------------
+In reversed order...
+5 4 3 2 1
+*/
