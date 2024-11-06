8cb8bfd5a4a45c9a590e822eaf458d0842a1ed18
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-06 10:45:00 +0000

    P6

--- Assignment 021/Program 06/main.c
+++ Assignment 021/Program 06/main.c
@@ -1,21 +1,36 @@
 /*
-f)  Declare an array of size 10. Take elements from the keyboard. 
-    Delete the element from a position (taken from user) and print the final array.
+f)  Declare an array of size 10. Take elements from the keyboard.
+    Delete the element from a position (taken from user) and print the final
+    array.
 */
 
 #include <stdio.h>
 int main() {
-    int sz=10;
+    int sz = 10;
     int a[sz];
-    printf("Enter 10 elements...");
-    for(int i=0;i<sz;i)
+    printf("Enter 10 elements...\n");
+    for (int i = 0; i < sz; i++) {
+        scanf("%d", &a[i]);
+    }
+    int pos;
+    printf("Enter position to delete : ");
+    scanf("%d", &pos);
+    for (int i = pos - 1; i < sz; i++) {
+        a[i] = a[i + 1];
+    }
+    sz--;
+    printf("After deleting, the array...\n");
+    for (int i = 0; i < sz; i++) {
+        printf("%d ", a[i]);
+    }
     return 0;
 }
 
 /*
 -----------Output-------------------------
-Enter 10 numbers...
-1 2 3 4 5 6 7 8 9 10
-Reversed array...
-10 9 8 7 6 5 4 3 2 1
+Enter 10 elements...
+11 12 31 4 50 62 75 82 9 100
+Enter position to delete : 6
+After deleting, the array...
+11 12 31 4 50 75 82 9 100
 */
