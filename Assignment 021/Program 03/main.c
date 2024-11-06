bc6e6148f792de38aa3812237b1874993c132b72
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-06 09:47:13 +0000

    x

---
+++ Assignment 021/Program 03/main.c
@@ -0,0 +1,26 @@
+/*
+c)  Print even index (like 2, 4,...) values and print sum of these values.
+*/
+
+#include <stdio.h>
+int main() {
+    int a[]={1,2,3,4,5,6,7,8};
+    
+    return 0;
+}
+
+/*
+-----------Output-------------------------
+Printing the values of int-array...
+Value : 1, Address : 0x7fffffffdd60
+Value : 2, Address : 0x7fffffffdd64
+Value : 3, Address : 0x7fffffffdd68
+Value : 4, Address : 0x7fffffffdd6c
+Value : 5, Address : 0x7fffffffdd70
+Printing the values of float-array...
+Value : 1.2, Address : 0x7fffffffdd40
+Value : 1.4, Address : 0x7fffffffdd44
+Value : 1.6, Address : 0x7fffffffdd48
+Value : 1.8, Address : 0x7fffffffdd4c
+Value : 2.0, Address : 0x7fffffffdd50
+*/
