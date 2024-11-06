eb84bbcc076eca7d5ddc2c28dcc08ce64ca0ac72
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-06 10:18:22 +0000

    P6

---
+++ Assignment 021/Program 06/main.c
@@ -0,0 +1,18 @@
+/*
+f)  Declare an array of size 10. Take elements from the keyboard. 
+    Delete the element from a position (taken from user) and print the final array.
+*/
+
+#include <stdio.h>
+int main() {
+    
+    return 0;
+}
+
+/*
+-----------Output-------------------------
+Enter 10 numbers...
+1 2 3 4 5 6 7 8 9 10
+Reversed array...
+10 9 8 7 6 5 4 3 2 1
+*/
