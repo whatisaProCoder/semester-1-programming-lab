13f327b32e50b8d62d06a3a2fd59397dbb6c7062
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-06 09:29:35 +0000

    P1

--- Assignment 021/Program 01/main.c
+++ Assignment 021/Program 01/main.c
@@ -1,16 +1,34 @@
 /*
-a)  Take n (number of elements in the array) from user and 
-    then take n elements (int or float) from user and store 
+a)  Take n (number of elements in the array) from user and
+    then take n elements (int or float) from user and store
     it in the array.
 */
 
 #include <stdio.h>
 int main() {
-    int n
+    int n;
+    printf("Enter n : ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter %d elements...\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    printf("Elements stored...\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", a[i]);
+    }
 }
 
 /*
 -----------Output------------
-In reversed order...
-5 4 3 2 1
+Enter n : 5
+Enter 5 elements...
+1 2 3 4 5
+Elements stored...
+1
+2
+3
+4
+5
 */
