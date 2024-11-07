4d51640d3ec205689711cc3c287927dd92baad1d
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-07 15:29:44 +0000

    shorten

--- Assignment 021/Program 02/main.c
+++ Assignment 021/Program 02/main.c
@@ -5,9 +5,9 @@ b)  Print the values of two arrays (one consists of
 
 #include <stdio.h>
 int main() {
-    int a[] = {1, 2, 3, 4, 5};
+    int a[] = {1, 2};
     int sza = sizeof(a) / sizeof(a[0]);
-    float b[] = {1.2, 1.4, 1.6, 1.8, 2.0};
+    float b[] = {1.2, 1.4};
     int szb = sizeof(b) / sizeof(b[0]);
     printf("Printing the values of int-array...\n");
     for (int i = 0; i < sza; i++) {
@@ -23,15 +23,9 @@ int main() {
 /*
 -----------Output-------------------------
 Printing the values of int-array...
-Value : 1, Address : 0x7fffffffdd60
-Value : 2, Address : 0x7fffffffdd64
-Value : 3, Address : 0x7fffffffdd68
-Value : 4, Address : 0x7fffffffdd6c
-Value : 5, Address : 0x7fffffffdd70
+Value : 1, Address : 0x7fffffffdd78
+Value : 2, Address : 0x7fffffffdd7c
 Printing the values of float-array...
-Value : 1.2, Address : 0x7fffffffdd40
-Value : 1.4, Address : 0x7fffffffdd44
-Value : 1.6, Address : 0x7fffffffdd48
-Value : 1.8, Address : 0x7fffffffdd4c
-Value : 2.0, Address : 0x7fffffffdd50
+Value : 1.2, Address : 0x7fffffffdd70
+Value : 1.4, Address : 0x7fffffffdd74
 */
