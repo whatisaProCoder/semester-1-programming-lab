da194cae0d065cfff2428e7030c8d5a740f8ee29
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-07 14:56:53 +0000

    P9

---
+++ Assignment 021/Program 09/main.c
@@ -0,0 +1,34 @@
+/*
+i)  Print all unique elements in an array.
+    If array arr = {2, 3, 2, 1, 4, 6, 4, 6}
+    Then output will be 3, 1
+*/
+
+#include <stdio.h>
+
+void elements(int a[], int sz) {
+    for (int i = 0; i < sz; i++) {
+        int j;
+        for (j = 0; j < sz; j++) {
+            if (i != j && a[i] == a[j]) {
+                break;
+            }
+        }
+        if (j == sz) {
+            printf("%d ", a[i]);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int a[] = {2, 3, 2, 1, 4, 6, 4, 6};
+    int sz = sizeof(a) / sizeof(a[0]);
+    elements(a, sz);
+    return 0;
+}
+
+/*
+-----------Output--------------
+3 1
+*/
