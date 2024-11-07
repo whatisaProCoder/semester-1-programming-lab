2360c73a12666f346aff282abe6a6a64d66b5a5a
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-07 15:15:42 +0000

    P11

---
+++ Assignment 021/Program 11/main.c
@@ -0,0 +1,20 @@
+/*
+k)  Print elements of array using recursion.
+*/
+
+#include <stdio.h>
+void printarr(int a[], int i, int sz) {
+    if (i == sz) return;
+    printf("%d ", a[i]);
+    printarr(a, i + 1, sz);
+}
+int main() {
+    int a[] = {2, 3, 4, 6, 7, 8};
+    int sz = sizeof(a) / sizeof(a[0]);
+    printarr(a, 0, sz);
+}
+
+/*
+-----------Output--------------
+2 3 4 6 7 8
+*/
