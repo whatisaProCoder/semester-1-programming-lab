9cf595eb7da01c01c9729a4c78ab1c4f9c21b302
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-12 05:11:22 +0000

    P3

---
+++ Assignment 022/Program 03/main.c
@@ -0,0 +1,34 @@
+/*
+c)  WAP in C to print individual characters in reverse order.
+*/
+
+#include <stdio.h>
+#include <string.h>
+void reverse(char *str, int len) {
+    char rev[len];
+    for (int i = 0; i < len; i++) {
+        rev[i] = str[len - 1 - i];
+    }
+    rev[len] = '\0';
+    strcpy(str, rev);
+}
+int main() {
+    char str[50];
+    printf("Enter a string...\n");
+    scanf("%s", str);
+
+    // reverse
+    int len = strlen(str);
+    reverse(str, len);
+
+    printf("Reversed string : %s\n", str);
+
+    return 0;
+}
+
+/*
+-----------Output------------
+Enter a string...
+DOG
+Reversed string : GOD
+*/
