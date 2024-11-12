1a7f97e96e39386a848c187b638bcb665dc90813
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2024-11-12 05:03:23 +0000

    P2

---
+++ Assignment 022/Program 02/main.c
@@ -0,0 +1,37 @@
+/*
+b)  WAP in C to sort a string array in ascending order.
+*/
+
+#include <stdio.h>
+#include <string.h>
+void sort(char *str, int len) {
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = 0; j < len - i - 1; j++) {
+            if (str[j] > str[j + 1]) {
+                char temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+}
+int main() {
+    char str[50];
+    printf("Enter a string...\n");
+    scanf("%s", str);
+
+    // sorting
+    int len = strlen(str);
+    sort(str, len);
+
+    printf("Sorted string : %s\n", str);
+
+    return 0;
+}
+
+/*
+-----------Output------------
+Enter a string...
+CAT
+Sorted string : ACT
+*/
